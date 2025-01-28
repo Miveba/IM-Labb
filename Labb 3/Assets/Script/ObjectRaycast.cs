8ff1323b7bf596fc929167a2ad7926e6b2f3ea08
Miveba <166626769+Miveba@users.noreply.github.com>
2025-01-28 11:22:03 +0100

    Muspekar fix

---
+++ Labb 3/Assets/Script/ObjectRaycast.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class RaycastForward : MonoBehaviour
+{
+    public Transform startObject;
+    public float rayDistance = 5f; 
+
+    void Update()
+    {
+        Vector3 direction = startObject.forward;
+        Ray ray = new Ray(startObject.position, direction);
+
+        Debug.DrawLine(startObject.position, startObject.position + direction * rayDistance, Color.red);
+
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, rayDistance))
+        {
+            Debug.Log("Raycast träffade: " + hit.collider.gameObject.name);
+        }
+    }
+}
