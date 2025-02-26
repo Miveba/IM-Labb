0492655db93cb2ee2ead637990b63e82c033daac
r202002020 <167029433+r202002020@users.noreply.github.com>
2025-02-26 16:12:20 +0100

    Monsterspawn och plane

--- Workshop 3/Assets/MonsterSpawn.cs
+++ Workshop 3/Assets/MonsterSpawn.cs
@@ -1,80 +1,55 @@
-/*
- * Copyright 2021 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
 
 public class MonsterSpawner : MonoBehaviour
 {
-    public DrivingSurfaceManager DrivingSurfaceManager;
-    public PackageBehaviour Package;
-    public GameObject PackagePrefab;
+    public GameObject monsterPrefab; // Monster-prefaben som ska spawna
+    public ARPlaneManager planeManager; // Hanterar AR-plan
 
+    private List<ARPlane> spawnedPlanes = new List<ARPlane>(); // Håller koll på plan som redan har monster
 
-
-
-    public static Vector3 RandomInTriangle(Vector3 v1, Vector3 v2)
+    private void OnEnable()
     {
-        float u = Random.Range(0.0f, 1.0f); 
-        float v = Random.Range(0.0f, 1.0f);
-        if (v + u > 1)
+        // Lyssna på eventet när nya plan upptäcks
+        planeManager.planesChanged += OnPlanesChanged;
+    }
+
+    private void OnDisable()
+    {
+        // Sluta lyssna när scriptet inaktiveras
+        planeManager.planesChanged -= OnPlanesChanged; 
+    }
+
+    private void OnPlanesChanged(ARPlanesChangedEventArgs args)
+    {
+        if (planeManager == null || planeManager.trackables.count == 0)
+            return;
+
+        // Hämta LockedPlane från DrivingSurfaceManager
+        DrivingSurfaceManager drivingSurfaceManager = FindObjectOfType<DrivingSurfaceManager>();
+        if (drivingSurfaceManager != null && drivingSurfaceManager.LockedPlane != null)
         {
-            v = 1 - v;
-            u = 1 - u;
-        }
+            ARPlane lockedPlane = drivingSurfaceManager.LockedPlane;
 
-        return (v1 * u) + (v2 * v);
-    }
-
-    public static Vector3 FindRandomLocation(ARPlane plane)
-    {
-        // Select random triangle in Mesh
-        var mesh = plane.GetComponent<ARPlaneMeshVisualizer>().mesh;
-        var triangles = mesh.triangles;
-        var triangle = triangles[(int)Random.Range(0, triangles.Length - 1)] / 3 * 3;
-        var vertices = mesh.vertices;
-        var randomInTriangle = RandomInTriangle(vertices[triangle], vertices[triangle + 1]);
-        var randomPoint = plane.transform.TransformPoint(randomInTriangle);
-
-        return randomPoint;
-    }
-
-    public void SpawnPackage(ARPlane plane)
-    {
-        var packageClone = GameObject.Instantiate(PackagePrefab);
-        packageClone.transform.position = FindRandomLocation(plane);
-
-        Package = packageClone.GetComponent<PackageBehaviour>();
-        Debug.Log($"Package: {Package}");
-    }
-
-    private void Update()
-    {
-        var lockedPlane = DrivingSurfaceManager.LockedPlane;
-        if (lockedPlane != null)
-        {
-            if (Package == null)
+            if (!spawnedPlanes.Contains(lockedPlane))
             {
-                SpawnPackage(lockedPlane);
+                SpawnMonster(lockedPlane);
+                spawnedPlanes.Add(lockedPlane);
             }
+        }
+    }
 
-            var packagePosition = Package.gameObject.transform.position;
-            packagePosition.Set(packagePosition.x, lockedPlane.center.y, packagePosition.z);
+
+
+    private void SpawnMonster(ARPlane plane)
+    {
+        if (monsterPrefab != null)
+        {
+            Vector3 spawnPosition = plane.transform.position; // Placera monstret på planet
+            GameObject monster = Instantiate(monsterPrefab, spawnPosition, Quaternion.identity);
+            monster.transform.SetParent(plane.transform); // Sätt planet som förälder så att det följer med
         }
     }
 }
--- Workshop 3/Assets/Starter Package/DrivingSurfaceManager.cs
+++ Workshop 3/Assets/Starter Package/DrivingSurfaceManager.cs
@@ -1,20 +1,4 @@
-﻿/*
- * Copyright 2021 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
@@ -25,26 +9,39 @@ public class DrivingSurfaceManager : MonoBehaviour
     public ARRaycastManager RaycastManager;
     public ARPlane LockedPlane;
 
-    [System.Obsolete]
-    public void LockPlane(ARPlane keepPlane)
-    {
-        // Disable all planes except the one we want to keep
-        var arPlane = keepPlane.GetComponent<ARPlane>();
-        foreach (var plane in PlaneManager.trackables)
-        {
-            if (plane != arPlane)
-            {
-                plane.gameObject.SetActive(false);
-            }
-        }
-
-        LockedPlane = arPlane;
-        PlaneManager.planesChanged += DisableNewPlanes;
-    }
+    private bool planeLocked = false; // 🔒 Håller koll på om vi redan har låst ett plan
 
     private void Start()
     {
         PlaneManager = GetComponent<ARPlaneManager>();
+        PlaneManager.planesChanged += OnPlanesChanged;
+    }
+
+    private void OnPlanesChanged(ARPlanesChangedEventArgs args)
+    {
+        if (planeLocked) return; // 🛑 Stoppa om vi redan har låst ett plan
+
+        foreach (var plane in args.added)
+        {
+            LockPlane(plane);
+            planeLocked = true; // 🔒 Markera att vi har låst ett plan
+            break; // 🚀 Avbryt loopen så att vi endast låser ett plan
+        }
+    }
+
+    public void LockPlane(ARPlane keepPlane)
+    {
+        LockedPlane = keepPlane;
+
+        foreach (var plane in PlaneManager.trackables)
+        {
+            if (plane != keepPlane)
+            {
+                plane.gameObject.SetActive(false); // ❌ Inaktivera andra plan
+            }
+        }
+
+        PlaneManager.planesChanged -= OnPlanesChanged; // ⛔ Sluta lyssna på nya plan
     }
 
     private void Update()
@@ -52,15 +49,6 @@ public class DrivingSurfaceManager : MonoBehaviour
         if (LockedPlane?.subsumedBy != null)
         {
             LockedPlane = LockedPlane.subsumedBy;
-        }
-    }
-
-    [System.Obsolete]
-    private void DisableNewPlanes(ARPlanesChangedEventArgs args)
-    {
-        foreach (var plane in args.added)
-        {
-            plane.gameObject.SetActive(false);
         }
     }
 }
