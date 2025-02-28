6aea61c99c4c786724be1d5f49dbcf02e2d6b6c0
r202002020 <167029433+r202002020@users.noreply.github.com>
2025-02-28 11:09:16 +0100

    flytta image tracked position

--- Workshop 3/Assets/Image Tracking/ImageRecognitionBehaviour.cs
+++ Workshop 3/Assets/Image Tracking/ImageRecognitionBehaviour.cs
@@ -1,35 +1,56 @@
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
-using UnityEngine.XR;
-using System.Collections;
 using System.Collections.Generic;
 
 public class ImageRecognitionBehaviour : MonoBehaviour
 {
     private ARTrackedImageManager _arTrackedImageManager;
+    public GameObject prefab; // Prefaben som ska placeras
+
+    private Dictionary<ARTrackedImage, GameObject> spawnedPrefabs = new Dictionary<ARTrackedImage, GameObject>();
 
     private void Awake()
     {
         _arTrackedImageManager = FindObjectOfType<ARTrackedImageManager>();
-        
     }
 
-    public void OnEnable()
+    private void OnEnable()
     {
         _arTrackedImageManager.trackedImagesChanged += OnImageChanged;
     }
 
-    public void OnDisable()
+    private void OnDisable()
     {
         _arTrackedImageManager.trackedImagesChanged -= OnImageChanged;
     }
 
-    public void OnImageChanged(ARTrackedImagesChangedEventArgs args)
+    private void OnImageChanged(ARTrackedImagesChangedEventArgs args)
     {
+        // Lägg till nya objekt när en bild trackas
         foreach (var trackedImage in args.added)
         {
-            Debug.Log(trackedImage.name);
+            var newPrefab = Instantiate(prefab, trackedImage.transform.position, trackedImage.transform.rotation);
+            spawnedPrefabs[trackedImage] = newPrefab;
+        }
+
+        // Uppdatera positionen och rotationen för trackade bilder
+        foreach (var trackedImage in args.updated)
+        {
+            if (spawnedPrefabs.TryGetValue(trackedImage, out GameObject spawnedPrefab))
+            {
+                spawnedPrefab.transform.position = trackedImage.transform.position;
+                spawnedPrefab.transform.rotation = trackedImage.transform.rotation;
+            }
+        }
+
+        // Ta bort objekt om bilden inte längre trackas
+        foreach (var trackedImage in args.removed)
+        {
+            if (spawnedPrefabs.TryGetValue(trackedImage, out GameObject spawnedPrefab))
+            {
+                Destroy(spawnedPrefab);
+                spawnedPrefabs.Remove(trackedImage);
+            }
         }
     }
-
 }
