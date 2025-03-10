5a211c4b01a1b5880ae0788b8f3a9d845efe1c74
r202002020 <167029433+r202002020@users.noreply.github.com>
2025-03-10 16:55:01 +0100

    sound

--- Workshop 3/Assets/AudioManager.cs
+++ Workshop 3/Assets/AudioManager.cs
@@ -4,7 +4,6 @@ public class AudioManager : MonoBehaviour
 {
     // Referens till AudioSource
     private AudioSource audioSource;
-    private AudioSource bg;
 
     // De tre ljudklippen
     public AudioClip soundClip1;
@@ -12,16 +11,13 @@ public class AudioManager : MonoBehaviour
     public AudioClip soundClip3;
     public AudioClip soundClip4;
     public AudioClip soundClip5;
-    public AudioClip backgroundMusic;
+ 
 
     // Startmetod där vi hämtar AudioSource komponenten
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
-        bg = GetComponent<AudioSource>();
-        bg.clip = backgroundMusic;
-        bg.loop = true;  // Sätt musikens loop till true
-        bg.Play();  // Börja spela musiken
+        
     }
 
     // Metod för att spela upp ljud 1
@@ -64,7 +60,7 @@ public class AudioManager : MonoBehaviour
         // Sätt pitch och volume innan vi spelar upp ljudet
         audioSource.pitch = pitch;
         audioSource.volume = volume;
-        audioSource.PlayOneShot(soundClip4); // Spela upp ljudklippet
+        audioSource.PlayOneShot(soundClip5); // Spela upp ljudklippet
     }
 
 }
